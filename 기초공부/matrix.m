65abfb39670b528eeb8e2073903b156fa5663bc5
Cpprhtn <63298243+cpprhtn@users.noreply.github.com>
2021-06-18 18:27:24 +0900

    기초 공부

---
+++ 기초공부/matrix.m
@@ -0,0 +1,23 @@
+% 행렬 생성
+a = [1,2,3];
+b = [1 2 3];
+c = [1;2;3];
+d = [1 2 3; 4 5 6; 7 8 9; 10 11 12];
+
+% 행렬 연산
+a = [1 2; 3 4];
+a + 1
+a - 1
+a * 2
+a / 2
+
+b = [3 4; 5 6];
+
+a + b
+a - b
+a * b % 행렬 곱셈
+a / b
+
+a .* b % 요소별 곱셈
+
+% 이외는 여기 참고 : https://kr.mathworks.com/help/matlab/matlab_prog/matlab-operators-and-special-characters.html
